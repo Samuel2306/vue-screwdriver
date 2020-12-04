f5b6ee15eb351e2f53f8c9ad60b5a04c8b2329c6
feng.shen <feng.shen@nio.com>
2020-12-04 18:32:05 +0800

    修改输出文件

--- src/plugins/lib/index.js
+++ src/plugins/lib/index.js
@@ -1,10 +1,16 @@
 import VueClipboard from 'vue-clipboard2'
 import VueScrollLock from 'v-scroll-lock'  // 在打开模态浮层的时候防止下层的元素滚动
 import VueClickOutside from 'v-click-outside'  // 在打开模态浮层的时候防止下层的元素滚动
+
+const install = function(Vue, options){
+  Vue.use(VueClipboard)
+  Vue.use(VueScrollLock)
+  Vue.use(VueClickOutside)
+}
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue);
+}
+
 export default {
-  install: function(Vue, options){
-    Vue.use(VueClipboard)
-    Vue.use(VueScrollLock)
-    Vue.use(VueClickOutside)
-  }
+  install
 }
