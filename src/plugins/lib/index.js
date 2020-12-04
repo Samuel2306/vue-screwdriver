ed94496d0c9f8e1d579bd08866ff0699d137dca9
feng.shen <feng.shen@nio.com>
2020-12-04 17:54:26 +0800

    引入第三方指令库v-click-outside

--- src/plugins/lib/index.js
+++ src/plugins/lib/index.js
@@ -1,8 +1,10 @@
 import VueClipboard from 'vue-clipboard2'
 import VueScrollLock from 'v-scroll-lock'  // 在打开模态浮层的时候防止下层的元素滚动
+import VueClickOutside from 'v-click-outside'  // 在打开模态浮层的时候防止下层的元素滚动
 export default {
   install: function(Vue, options){
     Vue.use(VueClipboard)
     Vue.use(VueScrollLock)
+    Vue.use(VueClickOutside)
   }
 }
