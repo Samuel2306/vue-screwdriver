8210f3b1f2170a4b43cf6082b788c9470ddb7579
feng.shen <feng.shen@nio.com>
2020-12-04 17:50:19 +0800

    引入第三方指令库v-scroll-lock

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,52 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h1>{{ test | swdFormatterEmptyData('----') }}</h1>
-    <h1>{{ test1 | swdFormatterMoney(formatterBefore, formatterAfter) }}</h1>
-    <h2>Essential Links</h2>
-    <div
-      id="sss"
-      style="width: 30%"
-      v-clamp={clamp:3}
-      v-clipboard:copy="test1"
-      v-clipboard:success="copy"
-      v-clipboard:error="onError">
-      很抱歉！没有搜索到相关模板很抱歉！没有搜索到相关模板很抱歉！没有搜索到相关模板很抱歉！没有搜索到相关模板word name
+  <div id="app" style="height: 100%;">
+    <button @click="onOpenModal">按钮</button>
+    <div class="modal" v-if="opened">
+      <button @click="onCloseModal">X</button>
+      <div class="modal-content" v-scroll-lock="opened">
+        <p>A bunch of scrollable modal content</p>
+      </div>
     </div>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div style="max-height: 100%; overflow: auto;">
+      <img src="./assets/logo.png">
+      <h1>{{ msg }}</h1>
+      <h1>{{ test | swdFormatterEmptyData('----') }}</h1>
+      <h1>{{ test1 | swdFormatterMoney(formatterBefore, formatterAfter) }}</h1>
+      <h2>Essential Links</h2>
+      <div
+        id="sss"
+        style="width: 30%"
+        v-clipboard:copy="test1"
+        v-clipboard:success="copy"
+        v-clipboard:error="onError">
+        很抱歉！没有搜索到相关模板很抱歉！没有搜索到相关模板很抱歉！没有搜索到相关模板很抱歉！没有搜索到相关模板word name
+        很抱歉！没有搜索到相关模板很抱歉！没有搜索到相关模板很抱歉！没有搜索到相关模板很抱歉！没有搜索到相关模板word name
+
+        很抱歉！没有搜索到相关模板很抱歉！没有搜索到相关模板很抱歉！没有搜索到相关模板很抱歉！没有搜索到相关模板word name
+
+        很抱歉！没有搜索到相关模板很抱歉！没有搜索到相关模板很抱歉！没有搜索到相关模板很抱歉！没有搜索到相关模板word name
+
+        很抱歉！没有搜索到相关模板很抱歉！没有搜索到相关模板很抱歉！没有搜索到相关模板很抱歉！没有搜索到相关模板word name
+
+        很抱歉！没有搜索到相关模板很抱歉！没有搜索到相关模板很抱歉！没有搜索到相关模板很抱歉！没有搜索到相关模板word name
+
+        很抱歉！没有搜索到相关模板很抱歉！没有搜索到相关模板很抱歉！没有搜索到相关模板很抱歉！没有搜索到相关模板word name
+
+      </div>
+      <ul>
+        <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+        <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+        <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
+        <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+      </ul>
+      <h2>Ecosystem</h2>
+      <ul>
+        <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+        <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+        <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+        <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -37,7 +57,8 @@ export default {
     return {
       msg: 'Welcome to Your Vue.js App',
       test: 0,
-      test1: 100.111
+      test1: 100.111,
+      opened: false
     }
   },
   methods: {
@@ -52,6 +73,14 @@ export default {
     },
     onError(e){
       console.log(e)
+    },
+
+    onOpenModal () {
+      this.opened = true
+    },
+
+    onCloseModal () {
+      this.opened = false
     }
   }
 }
@@ -64,7 +93,8 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+  position: relative;
 }
 
 h1, h2 {
@@ -84,4 +114,11 @@ li {
 a {
   color: #42b983;
 }
+  .modal{
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+  }
 </style>
--- src/plugins/lib/index.js
+++ src/plugins/lib/index.js
@@ -1,6 +1,8 @@
 import VueClipboard from 'vue-clipboard2'
+import VueScrollLock from 'v-scroll-lock'  // 在打开模态浮层的时候防止下层的元素滚动
 export default {
   install: function(Vue, options){
     Vue.use(VueClipboard)
+    Vue.use(VueScrollLock)
   }
 }
